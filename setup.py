ae60ce32fc145a66cbbcaafbcb591c478f19eb26
Eumeswil <hrittik.roy2014@gmail.com>
2018-10-15 19:32:20 +0530

    Setup fix

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name = 'skratch',version = '0.1',
-      url = https://github.com/sushantkumar23/skratch,
+setup(name = 'skratch',
+      version = '0.1',
+      url = "https://github.com/sushantkumar23/skratch",
       packages=find_packages(),
       install_requires=[
           'numpy','pandas','tensorflow'
