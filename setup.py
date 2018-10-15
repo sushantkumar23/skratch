9ec7a949e16b62ab48914440648d2d26bef9d947
Eumeswil <42641134+Eumeswil@users.noreply.github.com>
2018-10-15 19:15:37 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name = 'skratch',version = '0.1',
+      url = https://github.com/sushantkumar23/skratch,
+      packages=find_packages(),
+      install_requires=[
+          'numpy','pandas','tensorflow'
+      ],
+      include_package_data=True,
+      zip_safe=False)
+     
