a8100a4c333627a988ecf2f12456d9b0c74e0d6b
sushantkumar23 <sushantkumar23@gmail.com>
2018-10-16 12:31:48 +0530

    Removing the conflicts

--- skratch/agents/dqn_agent.py
+++ skratch/agents/dqn_agent.py
@@ -11,31 +11,23 @@ STACK_SIZE = 8
 
 
 class ReplayBuffer(object):
+    """
+    Replay Buffer which creates and stores the observations
+    at each time step and returns the state using the observation time
+    series
+    """
 
-<<<<<<< HEAD
-    def __init__(self,observation,action,reward):
-        self.past_observation =  self.observation
-        self.observation = observation
-        self.past_action = self.action
-        self.action = action
-        self.reward = reward
-        self.past_state = self.state
-        self.state = self.build_state()
-
-    def _get_time_features(self):
-        timestamp = self.observation[0]
-=======
     def __init__(
                 self,
                 replay_buffer_size=1000,
                 num_actions=3,
                 stack_size=8,
                 spread=0.00005):
-        """Initialises a Replay Buffer which creates and stores the observations
-        at each time step and returns the state using the observation time
-        series
+        """
+        Initialises a Replay Buffer .with the following parameters
 
-        The replay buffer takes care of generating the states from the observation
+        The replay buffer takes care of generating the states from the
+        observation
         """
 
         self.replay_buffer_size = replay_buffer_size
@@ -58,7 +50,7 @@ class ReplayBuffer(object):
             features generated using sin and cos encoding of the minute, hour
             and the weekday.
         """
->>>>>>> f-dqn
+
         min = timestamp.to_pydatetime().minute
         min_sin = np.sin(min*(2.*np.pi/60))
         min_cos = np.cos(min*(2.*np.pi/60))
@@ -79,31 +71,6 @@ class ReplayBuffer(object):
 
     def _get_market_features(self):
         """Returns the market feature at the current timestep"""
-<<<<<<< HEAD
-        log_ret = np.log(self.observation[1]) - np.log(self.past_observation[1])
-        market_features = np.append(market_features,log_ret)
-        market_features = market_features[-self.stack_size:]
-        return market_features
-
-    def _get_position_features(self, action = self.action):
-        """Returns the position feature based on the agent's last action"""
-        position_features = np.zeros(3)
-        position_features[action + 1] = 1
-        return position_features
-
-
-    # def build_state(self):
-    #     m = _get_market_features()
-    #     p = _get_position_feature()
-    #     t = _get_time_features()
-    #     state = np.append(m, p, t)
-    #     return state
-
-
-
-    def iniitalise_state(self, initial_observation):
-        """Initialises the time series with the first observation"""
-=======
         log_ret = np.log(self.time_series[1]) - np.log(self.time_series[2])
         market_features = market_features[-self.stack_size:]
         return market_features
@@ -121,7 +88,6 @@ class ReplayBuffer(object):
         is called in subsequent steps, they assume that last_states already
         has some values.
         """
->>>>>>> f-dqn
         self.time_series = pd.Series(observation[1], index=[observation[0]])
         time_features = self._get_time_features(observation[0])
         market_features = self._get_market_features()
@@ -132,34 +98,10 @@ class ReplayBuffer(object):
             state = np.concatenate((time_features, market_features, position_features))
             self.last_states.append(state)
 
-
-<<<<<<< HEAD
-
-    def get_experiences(self):
-        time_features = self._get_time_features(observation[0])
-        market_features = self._get_market_features()
-
-        log_return = np.log(self.time_series[-1]/self.time_series[-2])
-        self.next_states = []
-        experiences = []
-
-        for action in range(self.num_actions):
-            position_features = self.action_array[action]
-            next_state = np.concatenate((time_features, market_features, position_features))
-            self.next_states.append(next_state)
-            step_return = log_return * (action - 1)
-            for last_action, last_state in enumerate(self.last_states):
-                commission = self.spread * np.abs(action - last_action)
-                reward = step_return - commission
-                experiences.append((last_state, action, reward, next_state))
-
-        self.last_states = self.next_states
-=======
     def _append_time_series(self, observation):
         """Appends a single timestep to the time series"""
         new_series = pd.Series(observation[1], index=[observation[0]])
         self.time_series = pd.Series.concat([self.time_series, new_series])
-
 
     def store_observation(self, observation):
         """
@@ -206,10 +148,6 @@ class ReplayBuffer(object):
         batch = random.sample(self.__buffer, self.learning_timestep)
         return batch
 
->>>>>>> f-dqn
-
-        return experiences
-
 
 class DQNAgent(object):
     """
@@ -217,33 +155,6 @@ class DQNAgent(object):
     """
 
     def __init__(self,
-<<<<<<< HEAD
-                 replay_buffer_size = 1000,
-                 training_timestep = 96,
-                 stack_size = 8,
-                 gamma,
-                 spread=0.00005,
-                 action_size = 3,
-                 learning_rate = 0.00025,
-                 tau = 0.001
-                 ):
-        self.action_array = np.identity(3)
-        self.action_size = action_size
-        self._replay_buffer_size = replay_buffer_size
-        self._replay = collections.deque(maxlen=self._replay_buffer_size)
-        self.state
-        self.past_state
-        self.model1 = build_model()
-        self.model2 = build_model()
-        self.action
-        self.step = 0
-        self.training_timestep = training_timestep
-        self.learning_rate = learning_rate
-        self.tau = tau
-
-
-    def build_replay_buffer(self):
-=======
                 num_actions = 3,
                 gamma=0.99,
                 replay_buffer_size=1000,
@@ -290,48 +201,6 @@ class DQNAgent(object):
         self.action = None
         self.step = 0
 
-    def build_state(self, observation, action, reward):
-        """
-        Takes the observations and builds the state spaces out of the observations.
-        Parameters
-        ----------
-         observation (tuple):
-             observation is received from the runner and the environment.
-        """
-
-        #How does time feature work in our case is it even important
-        state = np.concatenate((time_features, market_features, position_features))
-        state = tf.convert_to_tensor(state, dtype = tf.float32)
-
-        return augmented_state
-
-    def action_augmentation(self, observation):
-
-        self.st.build_state(observation)
-        experiences = self.st.get_experiences(observation)
-        for experience in experiences:
-            self._replay.append(experience)
-
-    def _initialise_time_series(self, observation):
-        """Initialises the time series with the first observation"""
-        self.time_series = pd.Series(observation[1], index=[observation[0]])
-        time_features = self._get_time_features(observation[0])
-        market_features = self._get_market_features()
-
-        self.last_states = []
-        for action in range(self.num_actions):
-            position_features = self.action_array[action]
-            state = np.concatenate((time_features, market_features, position_features))
-            self.last_states.append(state)
-
-
-    def build_replay_buffer(self,reward,action):
->>>>>>> f-dqn
-        """Updates the replay buffer based on the new observations"""
-        self.replay_buffer = self.replay_buffer.append(self.st.action_augmentation() )
-        self.replay_buffer = self.replay_buffer[-replay_history:]
-
-
     def _build_network(self, name=None):
         """Returns a standard model for training the Q-network"""
         model = Sequential(name=name)
@@ -371,15 +240,6 @@ class DQNAgent(object):
         agent's next action
         """
         self.step += 1
-<<<<<<< HEAD
-        if self.step % self.training_timestep == 0:
-            self._train_model()
-        self._update_target_weights()
-        self.action = self._select_action()
-        self.past_state = self.state
-        self.st = StateTransformer(observation,reward,self.action)
-        build_replay_buffer(reward)
-=======
         self.action = self._select_action()
 
         # Add the observation to the replay buffer
@@ -388,7 +248,6 @@ class DQNAgent(object):
         # Perform the training of the networks
         self._train_step()
 
->>>>>>> f-dqn
         return self.action
 
     def end_episode(self, reward, observation):
@@ -416,37 +275,6 @@ class DQNAgent(object):
         act_value = self.online_network.predict(state)
         return np.argmax(act_value)
 
-
-<<<<<<< HEAD
-    def _build_model(self):
-        """Returns a standard model for training the Q-network"""
-        model = Sequential()
-        model.add(Dense(24, input_dim=self.state_size, activation='elu'))
-        model.add(Dense(24, activation='elu'))
-       # model.add(LSTM(1, input_shape = (24,1)))
-        model.add(Dense(self.action_size, activation='sigmoid'))
-        model.compile(loss='mse',
-                  optimizer=Adam(lr=self.learning_rate))
-        return model
-
-    def _train_model(self):
-        minibatch = random.sample(self.replay_buffer, self.learning_timestep)
-        for state, action, reward, next_state in minibatch :
-            Q_next = self.model2.predict(next_state)
-            a = argmax(self.model1.predict(next_state))
-            target = reward + self.gamma * Q_next[a]
-            #greedy action wrt model1 not model 2
-            #train network
-            self.model1.fit(state, target, epochs=1)
-
-    def _update_target_weights(self):
-        n = len(self.model1.layers)
-        w1 = model1.get_weights()
-        w2 = model2.get_weights()
-        for i in range(len(w2)):
-            w2 = (1 - self.tau)*w2 + self.tau * w1
-        self.model2.set_weights(w2)
-=======
     def _train_step(self):
         """Runs a single training step based on the update periods of both the
         online network
@@ -483,4 +311,3 @@ class DQNAgent(object):
         for i in range(len(w2)):
             w2 = (1 - self.tau)*w2 + self.tau * w1
         self.target_network.set_weights(w2)
->>>>>>> f-dqn
